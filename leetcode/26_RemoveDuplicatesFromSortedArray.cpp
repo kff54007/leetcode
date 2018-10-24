df0ae88489cf2356a790f641afcbda372278cd49
kff54007 <fanfu.kong000@gmail.com>
2018-10-24 10:57:27 +0800

    26_RemoveDuplicatesFromSortedArray

---
+++ leetcode/26_RemoveDuplicatesFromSortedArray.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+using std::vector;
+class Solution {
+public:
+	int removeDuplicates(vector<int>& nums) {
+		if (nums.size() == 0)
+			return 0;
+		int length = 1;
+		for (const auto& t:nums)
+		{
+			if (nums[length - 1] != t)
+				nums[length++] = t;
+		}
+		return length;
+	}
+};
