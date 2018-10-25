219c3d1f0220fcbd69c5da5b12c25ece02bf5fc8
kff54007 <fanfu.kong000@gmail.com>
2018-10-25 15:11:05 +0800

    28

--- leetcode/28_Implement_strStr().cpp
+++ leetcode/28_Implement_strStr().cpp
@@ -3,10 +3,13 @@ using std::string;
 class Solution28 {
 public:
 	//Brute-Force
+	size_t q;
 	int strStr(string haystack, string needle) {
 		if (needle.empty())
 			return 0;
-		for (int i = 0; i < haystack.size(); i++)
+		if (needle.size() > haystack.size())
+			return -1;
+		for (int i = 0; i < haystack.size() - needle.size() + 1; i++)
 		{
 			int j = 0;
 			for (; j < needle.size(); j++)
@@ -22,3 +25,9 @@ public:
 	//KMP
 	//...
 };
+int main()
+{
+	Solution28 a;
+	a.strStr("abb", "abaaa");
+	return 0;
+}
