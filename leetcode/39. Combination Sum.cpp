6794ef84c969b6479822f325d065c1bdc41ca566
fanfu.kong <kff54007@163.com>
2019-07-30 15:51:19 +0800

    leetcode 39

--- leetcode/39. Combination Sum.cpp
+++ leetcode/39. Combination Sum.cpp
@@ -3,6 +3,26 @@
 
 using std::vector;
 using std::sort;
+
+class trace {
+	int sum_ = 0;
+	vector<int> sk_;
+public:
+	void push(int s) {
+		sum_ += s;
+		sk_.emplace_back(s);
+	}
+	void pop() {
+		sum_ -=  sk_.back();
+		sk_.pop_back();
+	}
+	int sum() {
+		return sum_;
+	}
+	vector<int> one() {
+		return sk_;
+	}
+};
 class Solution39
 {
 public:
@@ -27,11 +47,40 @@ public:
 		find(ret, one, candidates, target, 0);
 		return ret;
 	}
+
+	vector<vector<int>> combinationSum2(vector<int>&& candidates, int target) {
+		sort(candidates.begin(), candidates.end());
+		vector<vector<int>> ret;
+		trace tr;
+		vector<int> level;
+		for (tr.push(0), level.emplace_back(-1); !level.empty(); level.pop_back(), tr.pop()) {
+			if (level.back() + 1 >= candidates.size()) {
+				continue;
+			}
+			level.back()++;
+			tr.pop();
+			tr.push(candidates[level.back()]);
+			while (true) {
+				if (tr.sum() < target) {
+					level.emplace_back(level.back());
+					tr.push(candidates[level.back()]);
+					continue;
+				}
+				if (tr.sum() == target) {
+					ret.emplace_back(tr.one());
+				}
+				break;
+			}
+		}
+		return ret;
+	}
 };
+
+
 
 int main()
 {
 	Solution39 a;
-	auto t = a.combinationSum({ 2,3,5 }, 8);
+	auto t = a.combinationSum2({ 2,3,5 }, 8);
 	return 0;
 }
